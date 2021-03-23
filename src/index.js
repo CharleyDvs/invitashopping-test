0e6356b99e008a0a2970909b7e32b542770e69eb
CharleyDvs <ing.carlosjaime@outlook.com>
2021-03-23 12:43:19 -0600

    First commit, app working but lacks styles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const productsMock = [
+  {
+    name: "Orange",
+    votes: 0,
+  },
+  {
+    name: "Banana",
+    votes: 0,
+  },
+];
+
+function GroceryApp({ products }) {
+  const [productsList, setProductsList] = useState(products);
+
+  const addVote = (products, name) => {
+    const newProductsList = products.map((product) => {
+      if (product.name.toLowerCase() === name.toLowerCase()) {
+        product.votes++;
+      }
+      return product;
+    });
+    setProductsList(newProductsList);
+  };
+
+  const downVote = (products, name) => {
+    const newProductsList = products.map((product) => {
+      if (
+        product.name.toLowerCase() === name.toLowerCase() &&
+        product.votes > 0
+      ) {
+        product.votes--;
+      }
+      return product;
+    });
+    setProductsList(newProductsList);
+    console.log("working");
+  };
+
+  return (
+    <div className="App">
+      <Products
+        products={productsList}
+        onVoteAdd={addVote}
+        onDownVote={downVote}
+      />
+    </div>
+  );
+}
+
+const Products = ({ products = [], onVoteAdd, onDownVote }) => {
+  const sortedArray = products.sort((a, b) => b.votes - a.votes);
+  return (
+    <ul>
+      {sortedArray.map((product, index) => (
+        <li key={`${product.name} - ${index}`}>
+          {" "}
+          <span>{product.name}</span> - <span>votes: {product.votes}</span>{" "}
+          <button
+            onClick={() => {
+              onVoteAdd(products, product.name);
+            }}
+          >
+            +
+          </button>{" "}
+          <button
+            onClick={() => {
+              onDownVote(products, product.name);
+            }}
+            disabled={product.votes === 0}
+          >
+            -
+          </button>{" "}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GroceryApp products={productsMock} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
